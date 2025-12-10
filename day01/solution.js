00c90f3e986e0cdcec873680f2dc44def857b532
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-10 17:18:33 +0100

    wip

--- day01/solution.js
+++ day01/solution.js
@@ -11,11 +11,18 @@ function rotateRight(start) {
 export function part1(input) {
   const lines = input.trim().split('\n');
   let start = 50;
+  let zeroes = 0;
   for(let line of lines) {
     let direction = line[0];
-    let number = 
+    let number = parseInt(line.slice(1), 10);
+    if (direction === 'L') {
+      start = rotateLeft(start, number);
+    } else if (direction === 'R') {
+      start = rotateRight(start, number);
+    }
+    if (start === 0) zeroes++;
   }
-  return sum;
+  return zeroes;
 }
 
 // export function part2(input) {
